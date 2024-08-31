95b45e8f994eb1195d6a320facb175e7486622c2
anngo-1 <andngo2004@gmail.com>
2024-08-30 21:20:15 -0700

    add boilerplate server

---
+++ eatery-server/src/index.ts
@@ -0,0 +1,16 @@
+import express, { urlencoded, json } from "express";
+
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "This is an's test server. It is up and running!!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
+
