bdd1b5621330f5b03427e51805955604aebf80f8
anngo-1 <andngo2004@gmail.com>
2024-10-05 01:07:35 -0700

    refactor testing file names, allow args on search.sh

--- eatery-server/search.sh
+++ eatery-server/search.sh
@@ -1,3 +1,13 @@
+# Default coordinates
+DEFAULT_LAT=32.9164
+DEFAULT_LNG=-117.1393
+DEFAULT_RADIUS=1609
+
+# Accept optional parameters for latitude and longitude
+LAT=${1:-$DEFAULT_LAT}
+LNG=${2:-$DEFAULT_LNG}
+
+# API call
 source .env
 curl -X POST -d '{
   "includedTypes": ["restaurant"],
@@ -5,9 +15,10 @@ curl -X POST -d '{
   "locationRestriction": {
     "circle": {
       "center": {
-        "latitude": 37.7937,
-        "longitude": -122.3965},
-      "radius": 500.0
+        "latitude": '"$LAT"',
+        "longitude": '"$LNG"'
+      },
+      "radius": '"$DEFAULT_RADIUS"'
     }
   }
 }' \
@@ -15,3 +26,4 @@ curl -X POST -d '{
 -H "X-Goog-Api-Key: ${APIKEY}" \
 -H "X-Goog-FieldMask: places.displayName" \
 https://places.googleapis.com/v1/places:searchNearby
+
